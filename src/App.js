9ee982a5dcdc5a1d69230ca928f44de528c66c56
Garry <gmisal2002@gmail.com>
2022-08-08 00:01:18 +0530

    done for today

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './Components/layout/Navbar';
+import Users from './Components/Users/Users';
+
 
 class App extends Component {
+
+  static defaultProps = {
+    title: "Github Finder"
+  };
+
   render() {
     return (
-      <div className='App'>App</div>
+      <div className='App'>
+        <Navbar title={"Github Finder"} />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
     )
   }
 }
