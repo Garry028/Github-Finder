036f19fbfc720bfe3d7ca83659474120afaf99c1
Garry <gmisal2002@gmail.com>
2022-08-07 20:15:31 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React, { Component } from 'react'
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div className='App'>App</div>
     )
   }
 }
+export default App;
